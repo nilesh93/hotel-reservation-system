0603599bcaee083bafdb8de59c70df221a649506
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2016-01-24 05:20:38 +0530

    Login, Registration complete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,15 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('Admin.Demo');
 });
+
+
+//---Auth
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+
+Route::post('auth/register', 'Auth\AuthController@postRegister');
