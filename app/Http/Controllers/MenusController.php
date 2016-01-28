a83f37c42bcee51f7dc5b1f0b32533357faf3783
Nipuna H Herath <hash.crackhead@gmail.com>
2016-01-28 09:28:49 +0530

    Changed admin sidebar ;)

--- app/Http/Controllers/MenusController.php
+++ app/Http/Controllers/MenusController.php
@@ -16,4 +16,23 @@ class MenusController extends Controller
         
     }
 
+    public function getPromotions(Request $request){
+    
+    $result = DB::table('promotions')->get();
+            return response()->json(['count' => count($result), 'data' => $result]);
+
+    }
+
+    public function getAddpromotion(Request $request){
+    $promotion_name = $request->input('promo_name');
+    $promotion_description = $request->input('promo_description');
+    $date_from = $request->input('promo_start');
+    $date_to = $request->input('promo_end');
+    $rate = $request->input('promo_rate');
+
+    DB::table('promotions')->insert(array('promotion_name'=> $promotion_name,'promotion_description'=> $promotion_description,'date_from'=> $date_from,'date_to'=> $date_to,'rate'=> $rate));
+    return 1;
+
+
+    }
 }
