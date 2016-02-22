d1c47c88bba75bea63ccc302da30c90ef9f2f214
Nipuna H Herath <hash.crackhead@gmail.com>
2016-02-22 06:18:32 +0530

    added detailed menus

--- resources/views/nipuna/menus.blade.php
+++ resources/views/nipuna/menus.blade.php
@@ -163,16 +163,44 @@ Menus
                             <label for="item" class="col-lg-5 control-label">Rate</label>
                             <div class="col-lg-6">
                                 <input class="form-control" id="menu_rate_edit" placeholder="Rate" type="text">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             </div>
                         </div>
                     </div>
 
-                    <div class="row">
+                    <!-- <div class="row">
                         <div class="form-group">
                             <label for="item" class="col-lg-5 control-label">Image</label>
                             <div class="col-lg-6">
                                 <img height="400" width="300" src={{URL::asset('BackEnd\assets\images\RoyalVictoriaMenu.jpg')}}></img>
                             </div>
+                        </div>
+                    </div> -->
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="portlet"> 
+
+<div class="portlet-body">
+ <div class="row" id="temptable">
+        <table class="table table-striped table-bordered table-hover dataTables-example" id="dm" plugin="datatable" >
+                    <thead>
+                        <tr>
+                            
+                            
+                            <th>Menu ID</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th class="col-md-1"></th>
+                            <th class="col-md-1"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+
+                </table>
+    </div>
+      </div></div>
                         </div>
                     </div>
                 </div>
@@ -185,7 +213,55 @@ Menus
                 <strong> Adding Fail!</strong>  
                 </div>
                 <div class="modal-footer">
-                    <button type="button" onclick="update_menu()" class="btn btn-primary" name="submit" >Update Promotion</button>
+                    <button type="button" onclick="update_menu()" class="btn btn-primary" name="submit" >Update Menu</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- END OF UPDATE MENUS MODAL -->
+    <div  class="modal fade" id="update_item_modal">
+        <div class="modal-dialog ">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title">Update Item</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" enctype="multipart/form-data" onsubmit="" method="post">
+
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="quantity" class="col-lg-5 control-label">Item Name</label>
+                            <div class="col-lg-6">
+                                <input class="form-control" id="item_name" placeholder="Item Name" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="quantity" class="col-lg-5 control-label">Item Price</label>
+                            <div class="col-lg-6">
+                                <input class="form-control" id="item_price" placeholder="Item Price" type="text">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            </div>
+                        </div>
+                    </div>
+    
+                </div>
+                <div class="alert alert-dismissible alert-success" id="addedsuccessfully" hidden="true">                   
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong> Updated Successfully!</strong>  
+                </div>
+                <div class="alert alert-dismissible alert-success" id="adderror" hidden="true">                   
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong> Updating Fail!</strong>  
+                </div>
+                <div class="modal-footer">
+                    <button type="button" onclick="update_menu()" class="btn btn-primary" name="submit" >Update Item Details</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     </form>
                 </div>
@@ -282,6 +358,7 @@ function add_menu(){
 }
 
 function edit(a){
+    
     console.log(a);
     $("#update_menus_modal").modal("show");
     console.log(a);
@@ -297,6 +374,7 @@ document.getElementById("menu_cat_edit").value = data[0].category;
 document.getElementById("menu_desc_edit").value = data[0].description;
 document.getElementById("menu_rate_edit").value = data[0].rate;
 document.getElementById("rownumber").value = a;
+detailedMenu(a);
 
             //swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
            // dataLoad();
@@ -361,9 +439,87 @@ function dataLoad(){
                 }
             ]
         } );
-
-
     }
+
+
+    function detailedMenu(a){
+
+        var oTable = $('#dm').DataTable();
+        oTable.destroy();
+
+        $('#dm').DataTable( {
+            "ajax": "admin_menus/detailedmenus?row="+a,
+            "columns": [
+                { "data": "menu_id" },
+                { "data": "item_name"},
+                { "data": "price"},
+                {"data" : null,
+                 "mRender": function(data, type, full) {
+                     return '<button class="btn btn-info  btn-animate btn-animate-side btn-block btn-sm" onclick="edit_items('+data.id+')"> Edit </button>' ;
+                 }
+                },
+                {"data" : null,
+                 "mRender": function(data, type, full) {
+                     return '<button class="btn btn-danger  btn-animate btn-animate-side btn-block btn-sm" onclick="delete_item('+data.id+')"> Delete </button>' ;
+                 }
+                }
+            ]
+        } );
+    }
+
+    function edit_items(a){
+    console.log(a);
+    $("#update_items_modal").modal("show");
+    console.log(a);
+
+    var data="row="+a;
+    $.ajax({
+    type:"get",
+    url:"admin_menus/retrieveitemdetails",
+    data:data,
+    success:function(data){
+      //  console.log(data[0].promotion_name);
+document.getElementById("item_name").value = data[0].item_name;
+document.getElementById("item_price").value = data[0].item_price;
+
+
+            //swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
+           // dataLoad();
+    
+    },
+    complete: function (data) {
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+    }    
+    });  
+}
+
+function delete_item(a){
+swal({   
+    title: "Are you sure?",   
+    text: "Are you sure you want to delete the record"+a+"?",   
+    type: "warning",   showCancelButton: true,   confirmButtonColor: "#DD6B55",   
+    confirmButtonText: "Yes, delete it!",   
+    closeOnConfirm: false }, 
+    function(){ 
+
+    var data="row="+a;
+    $.ajax({
+    type:"get",
+    url:"admin_menus/deleterow",
+    data:data,
+    success:function(data){
+            swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
+            dataLoad();
+    
+    },
+    complete: function (data) {
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+    }    
+    });  
+});
+}
 
 </script>
 
