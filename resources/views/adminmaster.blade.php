d17b2548499de6f0160b03dc5037c48614fee6bf
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2016-05-04 21:19:46 +0530

    Fixed admin master profile pics and some unwanted links.

--- resources/views/adminmaster.blade.php
+++ resources/views/adminmaster.blade.php
@@ -157,11 +157,11 @@
                             </li>
 
                             <li class="dropdown">
-                                <a href="" class="dropdown-toggle profile" data-toggle="dropdown" aria-expanded="true"><img src="BackEnd/assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle"> </a>
+                                <a href="" class="dropdown-toggle profile" data-toggle="dropdown" aria-expanded="true"><img src="BackEnd/assets/images/users/avatar-0.jpg" alt="user-img" class="img-circle"> </a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="javascript:void(0)"><i class="ti-user m-r-5"></i> Profile</a></li>
+                                    {{--<li><a href="javascript:void(0)"><i class="ti-user m-r-5"></i> Profile</a></li>
                                     <li><a href="javascript:void(0)"><i class="ti-settings m-r-5"></i> Settings</a></li>
-                                    <li><a href="javascript:void(0)"><i class="ti-lock m-r-5"></i> Lock screen</a></li>
+                                    <li><a href="javascript:void(0)"><i class="ti-lock m-r-5"></i> Lock screen</a></li>--}}
                                     <li><a href="{{URL::to('auth/logout')}}"><i class="ti-power-off m-r-5"></i> Logout</a></li>
                                 </ul>
                             </li>
