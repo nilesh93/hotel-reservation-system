b5b3733ea87c676c8d25145c1b9c6c6cafebbfcd
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-05-04 20:29:54 +0530

    adminmaster tweaked

--- resources/views/adminmaster.blade.php
+++ resources/views/adminmaster.blade.php
@@ -82,17 +82,7 @@
             <div class="navbar navbar-default" role="navigation">
                 <div class="container">
                     <div class="">
-                        <div class="pull-left">
-                            <button class="button-menu-mobile open-left">
-                                <i class="ion-navicon"></i>
-                            </button>
-                            <span class="clearfix"></span>
-                        </div>
-
-                        <form role="search" class="navbar-left app-search pull-left hidden-xs">
-                            <input type="text" placeholder="Search..." class="form-control">
-                            <a href=""><i class="fa fa-search"></i></a>
-                        </form>
+                         
 
                         <?php
                         $notification_count = DB::table('NOTIFICATIONS')
